1fdc3d71d5e3fad80078a53127b5b097f8e16b25
Tahmid Nishat <tahmid.ni7@gmail.com>
2019-02-18 21:11:57 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+(function($){
+    'use strict';
+    
+    jQuery(document).ready(function(){
+
+			     $(".home-page-slider").owlCarousel({
+			        items: 1,
+			        loop:true,
+			        nav: false,
+			       	dots:true,
+			       	autoplay: true
+
+			    });
+
+    });
+    
+    
+})(jQuery);
